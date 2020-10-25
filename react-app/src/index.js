1db00580da066ec160c3a81e4a9d6511413fb145
acio-o9 <aco6280113@gmail.com>
2020-10-25 20:35:41 +0900

    [add] monolith functional component

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,6 +4,28 @@ import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+function Comment(props) {
+    return (
+        <div className="Comment">
+            <div className="UserInfo">
+                <img className="Avatar"
+                    src={props.author.avatarUrl}
+                    alt={props.author.name}
+                />
+                <div className="UserInfo-name">
+                    {props.author.name}
+                </div>
+            </div>
+            <div className="Comment-text">
+                {props.text}
+            </div>
+            <div className="Comment-date">
+                {formatDate(props.date)}
+            </div>
+        </div>
+    );
+}
+
 class Welcome extends React.Component {
     render() {
         return <h1>Hello, {this.props.name}</h1>;
