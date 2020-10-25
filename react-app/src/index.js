1cb76f561bd006823699fe0efbc7d24c91b6d7fc
acio-o9 <aco6280113@gmail.com>
2020-10-25 23:56:56 +0900

    [clean] delete sample component

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,42 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-function Avatar(props) {
-    return (
-        <img className="Avatar"
-            src={props.user.avatarUrl}
-            alt={props.user.name}
-        />
-    );
-}
-
-function Comment(props) {
-    return (
-        <div className="Comment">
-            <div className="UserInfo">
-                <Avatar user={props.author} />
-                <div className="UserInfo-name">
-                    {props.author.name}
-                </div>
-            </div>
-            <div className="Comment-text">
-                {props.text}
-            </div>
-            <div className="Comment-date">
-                {formatDate(props.date)}
-            </div>
-        </div>
-    );
-}
-
-class Welcome extends React.Component {
-    render() {
-        return <h1>Hello, {this.props.name}</h1>;
-    }
-}
 
 function App() {
     return (
