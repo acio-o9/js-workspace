6cd3c48ebb177e43fae179bd988a3a1d57ed2521
acio-o9 <aco6280113@gmail.com>
2020-10-26 00:17:44 +0900

    [modify] rewrite sample clock with class component

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,19 +4,44 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function Clock(props) {
-    return (
-        <div>
-            <h1>Hello, world!</h1>
-            <h2>It is {props.date.toLocaleTimeString()}.</h2>
-        </div>
-    );
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
 }
 
 function tick() {
     ReactDOM.render(
-        <Clock date={new Date()} />,
-        document.getElementById('root'));
+        <Clock />,
+        document.getElementById('root')
+    );
 }
 
 setInterval(tick, 1000);
