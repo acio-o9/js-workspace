e34f55b73a4056eec1fc0917ebc2abc6b7a7a864
acio-o9 <aco6280113@gmail.com>
2020-10-25 20:38:02 +0900

    [add] Avatar component

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,14 +4,20 @@ import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+function Avatar(props) {
+    return (
+        <img className="Avatar"
+            src={props.user.avatarUrl}
+            alt={props.user.name}
+        />
+    );
+}
+
 function Comment(props) {
     return (
         <div className="Comment">
             <div className="UserInfo">
-                <img className="Avatar"
-                    src={props.author.avatarUrl}
-                    alt={props.author.name}
-                />
+                <Avatar user={props.author} />
                 <div className="UserInfo-name">
                     {props.author.name}
                 </div>
