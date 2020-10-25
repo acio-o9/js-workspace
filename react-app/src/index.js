e6b0ff45a58fd1b21fa609b504f8ac7e663176da
acio-o9 <aco6280113@gmail.com>
2020-10-25 23:57:42 +0900

    [modify] create sample clock with functional component

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,26 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function App() {
-    return (
+function tick() {
+    const element = (
         <div>
-            <Welcome name="Sara" />
-            <Welcome name="Cahal" />
-            <Welcome name="Edite" />
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
         </div>
     );
-}
-
-function tick() {
     ReactDOM.render(
-        <App />,
+        element,
         document.getElementById('root'));
 }
 
 setInterval(tick, 1000);
-
-
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
