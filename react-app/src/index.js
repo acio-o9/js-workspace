1c829925bd7082c01b5e7d76fddbe9c66d3cc795
acio-o9 <aco6280113@gmail.com>
2020-10-25 20:07:27 +0900

    [modify] to class component

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -17,8 +17,10 @@ function tick() {
 
 setInterval(tick, 1000);
 
-function Welcome(props) {
-    return <h1>Hello, {props.name}</h1>;
+class Welcome extends React.Component {
+    render() {
+        return <h1>Hello, {this.props.name}</h1>;
+    }
 }
 
 
