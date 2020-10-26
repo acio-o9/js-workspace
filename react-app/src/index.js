bd0ba6d72dcaae800ae51087252570cd4e299399
acio-o9 <aco6280113@gmail.com>
2020-10-26 20:30:34 +0900

    [clean] delete sample code

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,36 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-class Toggle extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {isToggle: true};
-        
-        // This binding is necessary to make `this` work in the callback
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick() {
-        this.setState(state => ({
-            isToggleOn: !state.isToggleOn
-        }));
-    }
-
-    render() {
-        return (
-            <button onClick={(e) => this.handleClick(e)}>
-                {this.state.isToggleOn ? 'ON' : 'OFF'}
-            </button>
-        );
-    }
-
-}
-
-ReactDOM.render(
-    <Toggle />,
-    document.getElementById('root')
-);
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
